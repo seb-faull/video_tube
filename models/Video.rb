6c2934c9be86a16da51140832ea6269762a55f57
seb-faull <seb_faull@hotmail.co.uk>
2017-10-01 16:02:36 +0100

    Set up hydrate method that turns string arguments to hashes. AND Select from index SQL method

--- models/Video.rb
+++ models/Video.rb
@@ -8,4 +8,41 @@ class Video
 	end
 
 
+	#Returns a new instance of the class; turns the strings in to hashes for controller
+	def self.hydrate video_data
+		video = Video.new
+
+		video.id = video_data['id']
+		video.title = video_data['title']
+		video.description = video_data['description']
+		video.url = video_data['url']
+		video.genre = video_data['genre']
+
+		video
+	end
+
+
+
+	#Index
+	def self.all
+		conn = self.open_connection
+		sql = "SELECT * FROM video;"
+		results = conn.exec(sql)
+
+		videos = results.map do |record|
+			self.hydrate(record)
+		end
+
+		videos
+	end
+
+
+
+
+
+
+
+
+
+
 end
