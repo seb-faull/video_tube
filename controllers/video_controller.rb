d610724a701be130cac599c27d325658a4c6235e
seb-faull <seb_faull@hotmail.co.uk>
2017-10-01 15:25:40 +0100

    Set up index and create routes

--- controllers/video_controller.rb
+++ controllers/video_controller.rb
@@ -9,6 +9,41 @@ class VideosController < Sinatra::Base
 	set :views, Proc.new { File.join(root, 'views') }
 
 
+	#Index
+	get "/videos" do
+		@page_header = "All videos"
+		@videos = Video.all
+		erb :"videos/index"
+	end
+
+
+
+	#Create
+	post "/videos" do
+		new_video = Video.new
+		new_video.title = params[:title]
+		new_video.description = params[:description]
+		new_video.url = params[:url]
+		new_video.genre = params[:genre]
+		new_video.save
+
+		redirect "/videos"
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
